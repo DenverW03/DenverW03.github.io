06deba9657414277a1ad941f156737de9735ab4d
DenverW03 <denverwhiteley@hotmail.com>
2024-08-26 12:41:25 +0100

    some desktop scaling

--- src/app/page.js
+++ src/app/page.js
@@ -8,9 +8,9 @@ export default function Home() {
 			<Navbar></Navbar>
 
       <div className={styles["hero-container"]}>
-        <div className="intro-text-box">
+        <div className={styles["intro-text-box"]}>
           <h1>Hi! I&apos;m Denver.</h1>
-          <div className="intro-text">
+          <div className={styles["intro-text"]}>
             I&apos;m a <b>full stack</b> developer who likes to work closely with you to understand what you need.<br />
             I like to produce an intuitive <b>user experience</b> to make services a pleasure to use.<br />
             I have experience in fields ranging from <b>robotics</b> to <b>full stack</b> applications.
