facb3e8bb9820c4d9d088ed71cd6304badf9ae0c
DenverW03 <denverwhiteley@hotmail.com>
2024-08-27 17:51:32 +0100

    experimenting

--- src/components/project-card/project-card.js
+++ src/components/project-card/project-card.js
@@ -1,9 +1,13 @@
 import styles from "./project-card.module.css";
 
-export default function ProjectCard({name, languages, link}) {
+export default function ProjectCard({name, description, languages, link}) {
 	return(
 		<div className={styles["card"]}>
-			
+			<h2>{name}</h2>
+			<div className={styles["project-desc-container"]}>
+				<p>{description}</p>
+				<p>Languages: {languages}</p>
+			</div>
 		</div>
 	);
 }
--- src/components/project-container/project-container.js
+++ src/components/project-container/project-container.js
@@ -2,11 +2,17 @@ import ProjectCard from "../project-card/project-card";
 import styles from "./project-container.module.css";
 
 export default function ProjectContainer() {
+	const cardData = [
+    { name: 'platform-syncer', description: 'A platform syncing tool for your videogames!!', languages: 'Rust, JavaScript, HTML, CSS', link: 'https://github.com/DenverW03/platform-syncer' },
+    { name: '', description: '', languages: '', link: '' },
+    { name: '', description: '', languages: '', link: '' },
+  ];
+
 	return(
 		<div className={styles["container"]}>
-			<ProjectCard />
-			<ProjectCard />
-			<ProjectCard />
+			{cardData.map((data) => (
+        <ProjectCard name={data.name} description={data.description} languages={data.languages} link={data.link} />
+      ))}
 		</div>
 	);
 }
